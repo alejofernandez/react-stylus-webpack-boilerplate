78c1477c6ca6d4efd060602313af1be94a31890a
Alejo Fernandez <alejofernandez@gmail.com>
2015-12-07 06:50:02 -0300

    Update 2015-12-07T09:49:59.136Z

--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export Footer from './Footer/Footer';
-export Header from './Header/Header';
-export Navbar from './Navbar/Navbar';
--- src/containers/index.js
+++
@@ -1,4 +0,0 @@
-export About    from './About/About';
-export App      from './App/App';
-export Home     from './Home/Home';
-export NotFound from './NotFound/NotFound';
